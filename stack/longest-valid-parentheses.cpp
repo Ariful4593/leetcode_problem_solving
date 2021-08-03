75d8e35332a367b55d3ce8fef19820515bce2945
Ariful4593 <arifkhagoria2015@gmail.com>
2021-08-03 12:09:58 +0600

    Find out valid 100 < longest parentheses

---
+++ stack/longest-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int longestValidParentheses(string s)
+{
+    int n = s.size();
+    stack <char> st;
+
+    st.push(-1);
+
+    int results = 0;
+    for(int i = 0; i < n; i++){
+        char c = s.at(i);
+        if(c == '('){
+            st.push(i);
+           }else{
+               st.pop();
+               if(st.empty()){
+                    st.push(i);
+               }else{
+                   results = i - st.top();
+               }
+           }
+    }
+    return results;
+}
+
+int main()
+{
+    string s = "(()";
+
+    cout << longestValidParentheses(s);
+}
