4c08bf3f3ef51e12c3e6a551bec9f7022bc89a16
Ariful4593 <arifkhagoria2015@gmail.com>
2021-08-08 14:20:44 +0600

    Basic calculator code updated

--- stack/basic_calculator.cpp
+++ stack/basic_calculator.cpp
@@ -1,137 +1,163 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-
-int operator_pecedence(char op)
+int operator_precedence(char ch)
 {
-    if(op == '+'){
+    if(ch == '(' || ch == ')')
+    {
+        return 0;
+    }
+    else if(ch == '+' || ch == '-')
+    {
         return 1;
-    }else if(op == '-'){
-        return 1;
-    }else if(op == '*'){
-        return 2;
-    }else{
+    }
+    else if(ch == '*' || ch == '/')
+    {
         return 2;
     }
 }
 
-int operation(int v1, int v2, char op)
+int operation(int a, int b, char ch)
 {
-    if(op == '+'){
-        return v1 + v2;
-    }else if(op == '-'){
-        return v1 - v2;
-    }else if(op == '+'){
-        return v1 * v2;
-    }else{
-        return v1 / v2;
+    if(ch == '+')
+    {
+        return b + a;
+    }
+    else if(ch == '-')
+    {
+        return b - a;
+    }
+    else if(ch == '*')
+    {
+        return b * a;
+    }
+    else if(ch == '/')
+    {
+        return b / a;
     }
 }
 
-
-int infix_expressions(string s)
+bool isNumber(const string& str)
 {
+    for (char const &c : str) {
+        if (isdigit(c) == 0){
+            return false;
+        }
+    }
+    return true;
+}
 
-    stack <int> operands;
-    stack <char> operat;
-    string temp = "";
+
+string infix_to_postfix(string s)
+{
+    stack <char> operators;
+    string postfix_expression = "";
+    s += ')';
+    operators.push('(');
     for(int i = 0; i < s.length(); i++){
 
-        if(s[i] == '(')
-        {
-            operat.push(s[i]);
-        }
-        else if(s[i] != '(' && s[i] != ')' && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/' && s[i] != ' ' && s[i] != ',')
-        {
-
-            temp = s[i];
-            if(s[i + 1] != '(' && s[i+1] != ')' && s[i+1] != '+' && s[i+1] != '-' && s[i+1] != '*' && s[i+1] != '/' && s[i+1] != ' ' && s[i+1] != ','){
-                temp += s[i + 1];
-
-                if(s.length() - 1 == i){
-                    break;
-                }else{
-                    long long number = 0;
-
-                    for(auto ch : temp)
-                    {
-                        number = (number * 10) + (ch - '0');
-                    }
-                    operands.push(number);
-                }
 
 
-            }else{
-                int cal = s[i] - 48;
-                operands.push(cal);
+        if(isdigit(s[i])){
+            int number = 0;
+            while(i < s.length() && isdigit(s[i])){
+                postfix_expression += s[i];
+                i++;
             }
-
+            postfix_expression += ",";
+            i--;
         }
-        else if(s[i] == ')')
+        else if(s[i] == '(')
         {
-            while(operat.top() != '(')
-            {
-                char operator_ = operat.top();
-                operat.pop();
-                int v2 = operands.top();
-                operands.pop();
-                int v1 = operands.top();
-                operands.pop();
-
-                int operation_value = operation(v1, v2, operator_);
-                operands.push(operation_value);
-            }
-            operat.pop();
+            operators.push(s[i]);
         }
         else if(s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' )
         {
-            while(operat.size() > 0 && operat.top() != '(' && operator_pecedence(s[i]) <= operator_pecedence(operat.top()))
+            if(!operators.empty() && operator_precedence(s[i]) > operator_precedence(operators.top()))
             {
-                char operator_ = operat.top();
-                operat.pop();
-                int v2 = operands.top();
-                operands.pop();
-                int v1 = operands.top();
-                operands.pop();
-
-                int operation_value = operation(v1, v2, operator_);
-                operands.push(operation_value);
+                operators.push(s[i]);
             }
-            operat.push(s[i]);
+            else if(!operators.empty() && operator_precedence(s[i]) <= operators.top())
+            {
+                char a = operators.top();
+                postfix_expression += a;
+                operators.pop();
+                operators.push(s[i]);
+            }
         }
+        else if(s[i] == ')')
+        {
+
+
+            while(operators.top() != '(')
+            {
+                char a = operators.top();
+                postfix_expression += a;
+                operators.pop();
+                postfix_expression += ",";
+
+            }
+
+            operators.pop();
+        }
+
     }
 
-    while(operat.size() != 0)
-    {
-        char operator_ = operat.top();
-        operat.pop();
-        int v2 = operands.top();
-        operands.pop();
-        int v1 = operands.top();
-        operands.pop();
-
-        int operation_value = operation(v1, v2, operator_);
-        operands.push(operation_value);
-    }
-
-    long long number = 0;
-
-    for(auto ch : s)
-    {
-        number = (number * 10) + (ch - '0');
-    }
-    cout << operands.size() << endl;
-    return operands.size() > 1 ? number : operands.top() ;
-
+    return postfix_expression;
 }
 
+int infix_notation(string s)
+{
+    stack <int> operands;
+    string postfix_expression = infix_to_postfix(s);
 
+
+    int number = 0;
+
+    if(isNumber(postfix_expression)){
+        for(auto ch : postfix_expression)
+        {
+            number = (number * 10) + (ch - '0');
+        }
+        return number;
+    }
+    else{
+
+        int a, b, result = 0;
+
+        for(int i = 0; i < postfix_expression.length(); i++)
+        {
+
+            if(isdigit(postfix_expression[i]))
+            {
+                int number = 0;
+                while(i < postfix_expression.length() && isdigit(postfix_expression[i])){
+                    number = (number * 10) + (postfix_expression[i] - '0');
+                    i++;
+                }
+                i--;
+                operands.push(number);
+            }
+            else if(!operands.empty() && postfix_expression[i] == '+' || postfix_expression[i] == '-' || postfix_expression[i] == '*' || postfix_expression[i] == '/' )
+            {
+                a = operands.top();
+                operands.pop();
+                b = operands.top();
+                operands.pop();
+                result = operation(a, b, postfix_expression[i]);
+                operands.push(result);
+            }
+        }
+        return result ? result : operands.top();
+    }
+}
 int main()
 {
-    //2147483647
-    string s = "2147483647";
+    //12/(7-3)+2*(1+5) working
+    // " 2-1 + 2 "  But it's not working
+    string s = " 2-1 + 2 ";
 
-    cout << infix_expressions(s);
+    cout << infix_notation(s);
 
-    return 0;
 }
