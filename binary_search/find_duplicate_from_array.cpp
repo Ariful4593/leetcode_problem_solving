ab94e5ed1995c35a0fc7305357b287b58f6d2fdf
Ariful4593 <ariful.og@gmail.com>
2021-08-19 18:34:47 +0600

    Find duplicate from array

---
+++ binary_search/find_duplicate_from_array.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int findDuplicate(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i < n; i++){
+        if(nums[i] == nums[i + 1]){
+            return nums[i];
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector <int> data;
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        data.push_back(a);
+    }
+
+    cout << findDuplicate(data);
+}
